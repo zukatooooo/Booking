f90784151d17ce8c2cd61e862ea42b92b2f9b96b
zukatooooo <absandzezuka406@gmail.com>
2024-04-23 12:52:47 +0400

    updated models

--- src/main/java/com/example/demo/model/Hotel.java
+++ src/main/java/com/example/demo/model/Hotel.java
@@ -1,4 +1,25 @@
 package com.example.demo.model;
 
 public class Hotel {
+    private int id;
+    private String name;
+    private int maxCapacity;
+
+    public Hotel(int id, String name, int maxCapacity) {
+        this.id = id;
+        this.name = name;
+        this.maxCapacity = maxCapacity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
 }
--- src/main/java/com/example/demo/model/Room.java
+++ src/main/java/com/example/demo/model/Room.java
@@ -1,4 +1,19 @@
 package com.example.demo.model;
 
 public class Room {
+    private int id;
+    private int hotelId;
+
+    public Room(int id, int hotelId) {
+        this.id = id;
+        this.hotelId = hotelId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getHotelId() {
+        return hotelId;
+    }
 }
